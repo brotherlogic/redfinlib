b70c4dca42b6417f3564bf5c7ca9a4934ae5155e
Simon Tucker <simon@brotherlogic@gmail>
2019-10-12 08:13:05 -0700

    Adds ability to extract sale state. This closes #6.

--- redfinlib.go
+++ redfinlib.go
@@ -12,6 +12,7 @@ import (
 type info struct {
 	Offers offers
 	Name   string
+	Price  int
 }
 
 type offers struct {
@@ -24,18 +25,51 @@ func jsonExtract(stats *pb.Stats, jsonStr string) {
 
 	if i.Offers.Price > 0 {
 		stats.CurrentPrice = int32(i.Offers.Price)
+		return
 	}
 
+	if i.Price > 0 {
+		stats.CurrentPrice = int32(i.Price)
+		return
+	}
+}
+
+func stateExtract(stats *pb.Stats, str string) {
+	re := regexp.MustCompile("Status:.*?clickable\">(.*?)<")
+
+	values := re.FindAllStringSubmatch(str, -1)
+	for _, val := range values {
+		if val[1] == "Sold" {
+			stats.State = pb.Stats_SOLD
+			return
+		}
+		if val[1] == "Pending" {
+			stats.State = pb.Stats_PENDING
+			return
+		}
+	}
 }
 
 func estimateExtract(stats *pb.Stats, str string) {
-	re := regexp.MustCompile("Redfin Estimate:.*?value\">\\$(.*?)<")
+	re := regexp.MustCompile("Redfin Estimate:.*?alue\">\\$(.*?)<")
 
 	values := re.FindAllStringSubmatch(str, -1)
 	for _, val := range values {
 		es, _ := strconv.Atoi(strings.Replace(val[1], ",", "", -1))
 		if es > 0 {
 			stats.CurrentEstimate = int32(es)
+			return
+		}
+	}
+
+	// Option 2
+	re = regexp.MustCompile("statsValue\">\\$(.*?)<.*?Redfin Estimate")
+	values = re.FindAllStringSubmatch(str, -1)
+	for _, val := range values {
+		es, _ := strconv.Atoi(strings.Replace(val[1], ",", "", -1))
+		if es > 0 {
+			stats.CurrentEstimate = int32(es)
+			return
 		}
 	}
 }
@@ -51,6 +85,7 @@ func Extract(data string) (*pb.Stats, error) {
 	}
 
 	estimateExtract(stats, data)
+	stateExtract(stats, data)
 
 	return stats, nil
 }
